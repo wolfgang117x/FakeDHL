7df76dd9044bc948354ece24e39fd74b728682e6
wolfgang101 <34808526+wolfgang101@users.noreply.github.com>
2019-12-01 21:09:08 +0530

    update password

--- package/models.py
+++ package/models.py
@@ -10,7 +10,7 @@ import random
 class dbconnection():
       def connect(self):
             try:
-                  self.con = psycopg2.connect("host=bdzm3eywimkxj1x3y5qv-postgresql.services.clever-cloud.com dbname=bdzm3eywimkxj1x3y5qv user=uthpua3wbyl5tmqd43x5 password=xJuNZNcLsTFnKI97m77O")
+                  self.con = psycopg2.connect("host=bkrumvo8yakpyk3ckmen-postgresql.services.clever-cloud.com dbname=bkrumvo8yakpyk3ckmen user=uthpua3wbyl5tmqd43x5 password=xJuNZNcLsTFnKI97m77O")
                   print(con)
                   self.cur = self.con.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor)
                   print(cur)
